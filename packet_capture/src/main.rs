e9c03b47da5a2b0f4b80180a52749bfee0be653d
dora256 <tanaka.219.game@gmail.com>
2021-01-04 11:44:21 +0000

    add get mac address

--- packet_capture/src/main.rs
+++ packet_capture/src/main.rs
@@ -1,9 +1,45 @@
 extern crate pnet;
 
-use pnet::datalink;
+use pnet::datalink::{self, NetworkInterface};
+use pnet::packet::ethernet::EthernetPacket;
+use pnet::datalink::Channel::Ethernet;
+
+fn handle_ethernet_frame(interface: &NetworkInterface, ethernet: &EthernetPacket) {
+    println!(
+        "{}: {} > {}",
+        interface.name,
+        ethernet.get_source(),
+        ethernet.get_destination(),
+    );
+}
 
 fn main() {
-    let interface = datalink::interfaces();
+    let iface_name = "eth0";
+    let interface_names_match = |iface: &NetworkInterface| iface.name == iface_name;
 
-    println!("{}", interface[2].name);
+    let interfaces = datalink::interfaces();
+    let interface = interfaces
+        .into_iter()
+        .filter(interface_names_match)
+        .next()
+        .unwrap_or_else(|| panic!("No such network interface: {}", iface_name));
+
+    println!("{:?}", interface);
+
+    let (_, mut rx) = match datalink::channel(&interface, Default::default()) {
+        Ok(Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("unhandled channel type: {}"),
+        Err(e) => panic!("unable to create channel: {}", e),
+    };
+
+    loop {
+        match rx.next() {
+            Ok(packet) => {
+                handle_ethernet_frame(&interface, &EthernetPacket::new(packet).unwrap())
+            }
+            Err(_) => {
+                continue;
+            }
+        }
+    }
 }
