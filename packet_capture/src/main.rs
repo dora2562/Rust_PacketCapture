4e16bacedce690287cc0e2c4024d37d0e3a1d3e5
dora256 <tanaka.219.game@gmail.com>
2021-01-04 13:46:26 +0000

    ver 0.1

--- packet_capture/src/main.rs
+++ packet_capture/src/main.rs
@@ -1,16 +1,43 @@
 extern crate pnet;
 
 use pnet::datalink::{self, NetworkInterface};
-use pnet::packet::ethernet::EthernetPacket;
 use pnet::datalink::Channel::Ethernet;
 
+use pnet::packet::Packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::udp::UdpPacket;
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::ip::{IpNextHeaderProtocol, IpNextHeaderProtocols};
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket};
+
 fn handle_ethernet_frame(interface: &NetworkInterface, ethernet: &EthernetPacket) {
-    println!(
-        "{}: {} > {}",
-        interface.name,
-        ethernet.get_source(),
-        ethernet.get_destination(),
-    );
+    match ethernet.get_ethertype() {
+        EtherTypes::Ipv4 => {
+            println!(
+                "{}: {} > {}", interface.name, ethernet.get_source(), ethernet.get_destination());
+            let ip = Ipv4Packet::new(ethernet.payload()).unwrap();
+            handle_ip_packet(&interface, &ip)
+        }
+        _ => (),
+    }
+}
+
+fn handle_ip_packet(interface: &NetworkInterface, ip: &Ipv4Packet) {
+    print!("    {}: ", ip.get_next_level_protocol());
+    match ip.get_next_level_protocol(){
+        IpNextHeaderProtocols::Tcp => {
+            let tcp = TcpPacket::new(ip.payload()).unwrap();
+            println!("{}:{} -> {}:{}", ip.get_source(), tcp.get_source(), ip.get_destination(), tcp.get_destination());
+        }
+        IpNextHeaderProtocols::Udp => {
+            let udp = UdpPacket::new(ip.payload()).unwrap();
+            println!("{}:{} -> {}:{}", ip.get_source(), udp.get_source(), ip.get_destination(), udp.get_destination());
+        }
+        IpNextHeaderProtocols::Icmp => {
+            println!("{} -> {}", ip.get_source(), ip.get_destination());
+        }
+        _ => (),
+    }
 }
 
 fn main() {
