2f1c025e5b41350a6b0f580e1670017270033374
tanaka <tanaka.219.game@gmail.com>
2021-01-05 05:10:14 +0000

    ver 0.1.2

--- packet_capture/src/main.rs
+++ packet_capture/src/main.rs
@@ -8,7 +8,31 @@ use pnet::packet::tcp::TcpPacket;
 use pnet::packet::udp::UdpPacket;
 use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::ip::{IpNextHeaderProtocol, IpNextHeaderProtocols};
-use pnet::packet::ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket};
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use std::collections::VecDeque;
+
+#[derive(Clone, Debug)]
+struct PacketWithInterface {
+    interface: NetworkInterface,
+    packet: Vec<u8>,
+}
+
+fn get_packet_id(interface: &NetworkInterface, ethernet: &EthernetPacket) -> u16 {
+    let ip = Ipv4Packet::new(ethernet.payload()).unwrap();
+    match ip.get_next_level_protocol(){
+        IpNextHeaderProtocols::Tcp => {
+            return ip.get_identification()
+        }
+        IpNextHeaderProtocols::Udp => {
+            return ip.get_identification()
+        }
+        IpNextHeaderProtocols::Icmp => {
+            return ip.get_identification()
+        }
+        _ => (),
+    }
+    return 0;
+}
 
 fn handle_ethernet_frame(interface: &NetworkInterface, ethernet: &EthernetPacket) {
     match ethernet.get_ethertype() {
@@ -28,10 +52,12 @@ fn handle_ip_packet(interface: &NetworkInterface, ip: &Ipv4Packet) {
         IpNextHeaderProtocols::Tcp => {
             let tcp = TcpPacket::new(ip.payload()).unwrap();
             println!("{}:{} -> {}:{}", ip.get_source(), tcp.get_source(), ip.get_destination(), tcp.get_destination());
+            println!("{}", ip.get_identification())
         }
         IpNextHeaderProtocols::Udp => {
             let udp = UdpPacket::new(ip.payload()).unwrap();
             println!("{}:{} -> {}:{}", ip.get_source(), udp.get_source(), ip.get_destination(), udp.get_destination());
+            println!("{}", ip.get_identification())
         }
         IpNextHeaderProtocols::Icmp => {
             println!("{} -> {}", ip.get_source(), ip.get_destination());
@@ -41,6 +67,7 @@ fn handle_ip_packet(interface: &NetworkInterface, ip: &Ipv4Packet) {
 }
 
 fn main() {
+    let mut v = VecDeque::new();
     let capture_iface_name = "eth0";
     let interface_names_match = |iface: &NetworkInterface| iface.name == capture_iface_name;
 
@@ -60,7 +87,13 @@ fn main() {
     loop {
         match rx.next() {
             Ok(packet) => {
-                handle_ethernet_frame(&interface, &EthernetPacket::new(packet).unwrap())
+                v.push_front(
+                    PacketWithInterface {
+                        interface: interface.clone(),
+                        packet: packet.to_owned(),
+                    }
+                );
+                //handle_ethernet_frame(&interface, &EthernetPacket::new(packet).unwrap())
             }
             Err(_) => {
                 continue;
